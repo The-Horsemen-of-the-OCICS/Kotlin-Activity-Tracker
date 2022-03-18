7424ba31ed53d58a93430ff075e7ac7beb128f9f
Luwei Cai <90882967+lcai006@users.noreply.github.com>
2022-03-18 01:05:39 -0400

    update model constructor

--- app/src/main/java/com/ocics/activitytracker/Activity.kt
+++ app/src/main/java/com/ocics/activitytracker/Activity.kt
@@ -1,11 +1,5 @@
 package com.ocics.activitytracker
 
-class Activity {
-    var type: String = ""
-    var startTime: String = ""
+class Activity(var type: String, var startTime: String) {
 
-    constructor(type: String, startTime: String) {
-        this.type = type
-        this.startTime = startTime
-    }
 }
